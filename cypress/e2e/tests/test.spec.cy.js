8e37f14bf9a54e49d5499065db6e4c64dffcc66c
Oghenekome Edaware <edawarekome@gmail.com>
2024-07-27 09:30:43 +0100

    cypress

---
+++ cypress/e2e/tests/test.spec.cy.js
@@ -0,0 +1,47 @@
+import {faker} from '@faker-js/faker'
+
+describe('cypress script assignment', () => {
+
+  
+    it('using arbitrary data to fill the form', () => {
+      cy.get('a[href*="/input-form-demo"]').should('be.visible').click()
+      cy.get('.wrapper h1').should('be.visible')
+      cy.get('#name').type('Mary Jane')
+      cy.get('[name="email"]').eq(1).type('maryjane@gmail.com')
+      cy.get('[name="password"]').type('Test123@')
+      cy.get('#company').type('Test Inc')
+      cy.get('#websitename').type('https://testinc.com')
+      cy.get('[name="country"]').select('BE')
+      cy.get('[name="city"]').type('Mainland')
+      cy.get('[name="address_line1"]').type('Lagos road 1')
+      cy.get('[name="address_line2"]').type('Lagos road 2')
+      cy.get('#inputState').type('Lagos')
+      cy.get('#inputZip').type('102400')
+      cy.get('#seleniumform button[type="submit"]').click()
+
+    })
+
+    it.only('using unique data to fill the form', () => {
+        cy.get('a[href*="/input-form-demo"]').should('be.visible').click()
+        cy.get('.wrapper h1').should('be.visible')
+        cy.get('#name').type(faker.person.fullName())
+        cy.get('[name="email"]').eq(1).type(faker.internet.email())
+        cy.get('[name="password"]').type(faker.internet.password())
+        cy.get('#company').type(faker.company.name())
+        cy.get('#websitename').type(faker.internet.domainName())
+        cy.get('[name="country"]').select('NG')
+        cy.get('[name="city"]').type(faker.location.city())
+        cy.get('[name="address_line1"]').type(faker.location.streetAddress())
+        cy.get('[name="address_line2"]').type(faker.location.streetAddress())
+        cy.get('#inputState').type(faker.location.state())
+        cy.get('#inputZip').type(faker.location.zipCode())
+        cy.get('#seleniumform button[type="submit"]').click()
+        cy.on('window:alert', (str) => {
+            expect(str).to.equal('Save address?');
+          });
+          cy.get('#save').click();
+          cy.get('.wrapper h1').should('be.visible')
+  
+  })
+})
+  //do something wit the alert
